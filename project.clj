dbe901c2d26f5b2a2e0c460a00f29e40b16c9abe
franks42 <frank.siebenlist@gmail.com>
2012-05-02 13:22:04 -0700

    updated for clj-info "0.2.3"

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Clojure interface to the "growlnotify" command line tool to post growl notifications"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [hiccup "0.3.8"]
-                 [clj-info "0.2.3-SNAPSHOT"]]
+                 [clj-info "0.2.3"]]
   :main clj-growlnotify.gdoc)
