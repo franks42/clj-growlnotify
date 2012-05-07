9157e972b66840725814b2111f209a3c6531e52e
franks42 <frank.siebenlist@gmail.com>
2012-05-07 11:54:23 -0700

    added license info to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clj-growlnotify "0.1.2-SNAPSHOT"
   :description "Clojure interface to the "growlnotify" command line tool to post growl notifications"
+  :url "https://github.com/franks42/clj-growlnotify"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [hiccup "0.3.8"]
                  [clj-info "0.2.3"]]
