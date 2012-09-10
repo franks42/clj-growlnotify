a23f19df792e66518a43d2b8fa50bc0f84a2caf4
franks42 <frank.siebenlist@gmail.com>
2012-09-09 22:20:33 -0700

    bumped dependencies in project.clj to latest&greatest

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/franks42/clj-growlnotify"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [hiccup "0.3.8"]
-                 [clj-info "0.2.3"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hiccup "1.0.1"]
+                 [clj-info "0.2.4"]]
   :main clj-growlnotify.gdoc)
